9b4e1c89f2768e960c79174dbc8f612e3f142039
mm-sakshig <138591084+mm-sakshig@users.noreply.github.com>
2023-07-04 18:16:41 +0530

    Add files via upload

---
+++ Repositories/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using LinQ_Prac.Models;
+
+namespace LinQ_Prac.Repositories
+{
+    public interface IUserRepository
+    {
+        IEnumerable<UserModel> GetUsers();
+        UserModel GetUserById(int userId);
+        void InsertUser(UserModel user);
+        void DeleteUser(int userId);
+        void UpdateUser(UserModel user);
+    }
+}
---
+++ Repositories/UserRepository.cs
@@ -0,0 +1,79 @@
+﻿using LinQ_Prac.Data;
+using LinQ_Prac.Models;
+using Microsoft.Azure.Documents;
+
+namespace LinQ_Prac.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private ApplicationDbContext _dataContext;
+
+        public UserRepository(ApplicationDbContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        public void DeleteUser(int userId)
+        {
+            UserModel user = _dataContext.User.Where(u => u.Id == userId).SingleOrDefault();
+            _dataContext.User.Remove(user);
+            _dataContext.SaveChanges();
+        }
+
+        public UserModel GetUserById(int userId)
+        {
+            var query = from u in _dataContext.User
+                        where u.Id == userId
+                        select u;
+            var user = query.FirstOrDefault();
+            var model = new UserModel()
+            {
+                Id = userId,
+                Name = user.Name,
+                Email = user.Email,
+                Age = user.Age
+            };
+            return model;
+        }
+
+        public IEnumerable<UserModel> GetUsers()
+        {
+            IList<UserModel> userList = new List<UserModel>();
+            var query = from user in _dataContext.User
+                        select user;
+            var users = query.ToList();
+            foreach (var userData in users)
+            {
+                userList.Add(new UserModel()
+                {
+                    Id = userData.Id,
+                    Name = userData.Name,
+                    Email = userData.Email,
+                    Age = userData.Age
+                });
+            }
+            return userList;
+        }
+
+        public void InsertUser(UserModel user)
+        {
+            var userData = new UserModel()
+            {
+                Name = user.Name,
+                Email = user.Email,
+                Age = user.Age
+            };
+            _dataContext.User.Add(userData);
+            _dataContext.SaveChanges();
+        }
+
+        public void UpdateUser(UserModel user)
+        {
+            UserModel userData = _dataContext.User.Where(u => u.Id == user.Id).SingleOrDefault();
+            userData.Name = user.Name;
+            userData.Email = user.Email;
+            userData.Age = user.Age;
+            _dataContext.SaveChanges();
+        }
+    }
+}
