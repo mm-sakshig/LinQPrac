79cc1c0e9964621a3ad6d587d77274dd2744e288
mm-sakshig <138591084+mm-sakshig@users.noreply.github.com>
2023-07-04 18:17:40 +0530

    Add files via upload

---
+++ Controllers/UserController.cs
@@ -0,0 +1,99 @@
+﻿using LinQ_Prac.Models;
+using LinQ_Prac.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+
+namespace LinQ_Prac.Controllers
+{
+    public class UserController : Controller
+    {
+        private IUserRepository _repository;
+
+        public UserController(IUserRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public IActionResult Index()
+        {
+            var users = _repository.GetUsers();
+            return View(users);
+        }
+
+        public ActionResult Create()
+        {
+            return View(new UserModel());
+        }
+        [HttpPost]
+        public ActionResult Create(UserModel user)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _repository.InsertUser(user);
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (DataException)
+            {
+                ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
+            }
+            return View(user);
+        }
+
+        public ActionResult Details(int id)
+        {
+            UserModel model = _repository.GetUserById(id);
+            return View(model);
+        }
+        public ActionResult Edit(int id)
+        {
+            UserModel model = _repository.GetUserById(id);
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult Edit(UserModel user)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _repository.UpdateUser(user);
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (DataException)
+            {
+                ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
+            }
+            return View(user);
+        }
+        public ActionResult Delete(int id, bool? saveChangesError)
+        {
+            if (saveChangesError.GetValueOrDefault())
+            {
+                ViewBag.ErrorMessage = "Unable to save changes. Try again, and if the problem persists see your system administrator.";
+            }
+            UserModel user = _repository.GetUserById(id);
+            return View(user);
+        }
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            try
+            {
+                UserModel user = _repository.GetUserById(id);
+                _repository.DeleteUser(id);
+            }
+            catch (DataException)
+            {
+                return RedirectToAction("Delete",
+                new RouteValueDictionary {
+                  { "id", id },
+                  { "saveChangesError", true } });
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using LinQ_Prac.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LinQ_Prac.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public ApplicationDbContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+        }
+
+        public DbSet<UserModel> User { get; set; }
+    }
+}
