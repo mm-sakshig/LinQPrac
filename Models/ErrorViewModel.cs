0d394b8a3eae05e2f172cb9d9ea2b6a311cd0cb9
mm-sakshig <138591084+mm-sakshig@users.noreply.github.com>
2023-07-04 18:17:24 +0530

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LinQ_Prac.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/UserModel.cs
@@ -0,0 +1,10 @@
+﻿namespace LinQ_Prac.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+    }
+}
